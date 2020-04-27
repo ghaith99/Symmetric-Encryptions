8f0a154a3293e725d1433cb81bb962f1a02d7a09
Ghaith AlGhaith <gh4ith@gmail.com>
2020-04-27 04:44:59 +0300

    Working Text Hash

--- Hashing/Text_Hash.cs
+++ Hashing/Text_Hash.cs
@@ -1,40 +1,88 @@
 using System;
-using System.IO;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Text;
 
-					
 public class Program
 {
+
+    public enum EncryptionEncoding{Default, ANSI, Unicode, BigEndianUnicode, UTF8}
+	public enum HashAlgorithmForAction { MD5, RIPEMD160, SHA1, SHA256, SHA384, SHA512 }
+	public static Dictionary<EncryptionEncoding, Encoding> EncodingMapping;
+
 	public static void Main()
 	{
+        MapEncryptionEncodingEnums();
 		
-        String textToHash=new TextString(@"""Hello Hash""");
-        String hashedText=String.CreateString(_Variables["hashedtext"]);
-        HashText( textToHash,ref hashedText, hashAlgorithm: 0, encoding: 2);
+        String textToHash= "Hello World";
+        String hashedText= "";
+        HashText( textToHash,ref hashedText, hashAlgorithm: 0, encoding: (int)EncryptionEncoding.UTF8);
 
 		Console.WriteLine(hashedText);	
 		
 	}
 
-public static void HashText(Variant textToHash, ref Variant hashedText, int hashAlgorithm, int encoding)
-{
-	TextVariant textVariant = (TextVariant)ActionRuntimeBase.CheckType(textToHash, "Text to Hash", false, typeof(TextVariant));
-	try
-	{
-		byte[] bytes = CryptographyActions.EncodingMapping[(EncryptionEncoding)encoding].GetBytes(textVariant.ToString(CultureInfo.InvariantCulture));
-		byte[] inArray = CryptographyActions.HashDataImp((HashAlgorithmForAction)hashAlgorithm, bytes);
-		if (hashedText != null)
+
+
+    public static void HashText(String textToHash, ref String hashedText, int hashAlgorithm, int encoding)
+    {
+        try
+        {
+            byte[] bytes = EncodingMapping[(EncryptionEncoding)encoding].GetBytes(textToHash);
+            byte[] inArray = HashDataImp((HashAlgorithmForAction)hashAlgorithm, bytes);
+            if (hashedText != null)
+            {
+                hashedText = Convert.ToBase64String(inArray);
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex);
+        }
+    }
+
+	private static byte[] HashDataImp(HashAlgorithmForAction hashAlgorithm, byte[] dataBytes)
 		{
-			hashedText = new TextVariant(Convert.ToBase64String(inArray));
+			byte[] result;
+			using (HashAlgorithm hashAlgorithm2 = GetHashAlgorithm(hashAlgorithm))
+			{
+				result = hashAlgorithm2.ComputeHash(dataBytes);
+				hashAlgorithm2.Clear();
+			}
+			return result;
 		}
-	}
-	catch (Exception ex)
-	{
-		if (ex is IWARuntimeActionException)
+
+//Helpers
+		private static HashAlgorithm GetHashAlgorithm(HashAlgorithmForAction hashAlgorithm)
 		{
-			throw;
+			switch (hashAlgorithm)
+			{
+			case HashAlgorithmForAction.MD5:
+				return new MD5Cng();
+			case HashAlgorithmForAction.RIPEMD160:
+				return new RIPEMD160Managed();
+			case HashAlgorithmForAction.SHA1:
+				return new SHA1Cng();
+			case HashAlgorithmForAction.SHA256:
+				return new SHA256Cng();
+			case HashAlgorithmForAction.SHA384:
+				return new SHA384Cng();
+			case HashAlgorithmForAction.SHA512:
+				return new SHA512Cng();
+			default:
+				throw new InvalidOperationException();
+			}
 		}
-		throw new ActionException(1, "Failed to hash text", ex);
-	}
-}
+
+	public static void MapEncryptionEncodingEnums()
+	{
+      //mapping to enum values as the Encoding are not enums (ex.Encoding.Default is a function)
+		EncodingMapping = new Dictionary<EncryptionEncoding, Encoding>();
+		EncodingMapping[EncryptionEncoding.Default] = Encoding.Default;
+		EncodingMapping[EncryptionEncoding.ANSI] = Encoding.ASCII;
+		EncodingMapping[EncryptionEncoding.Unicode] = Encoding.Unicode;
+		EncodingMapping[EncryptionEncoding.BigEndianUnicode] = Encoding.BigEndianUnicode;
+		EncodingMapping[EncryptionEncoding.UTF8] = Encoding.UTF8;  
+    }
 
 }
